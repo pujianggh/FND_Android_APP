1518ab77b11fa232ec434574bcc28cee2b4470d2
蒲江 <515210530@163.com>
2017-09-02 23:27:19 +0800

    提交打印日志

--- app/src/main/java/com/fengniao/action/MainActivity.java
+++ app/src/main/java/com/fengniao/action/MainActivity.java
@@ -1,13 +1,17 @@
 package com.fengniao.action;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_main);
+        Log.d("","");
     }
 }
